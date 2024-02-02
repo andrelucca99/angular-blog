a0051262c13d52c97bc0d10479a458d625ae4be6
André Lucas <andrelucca99@gmail.com>
2024-02-02 10:52:29 -0300

    feat: add page de content e dados fakes

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Tecnologias de front-end: 6 maneiras de comparar Angular e React",
+    "description": "Este é um artigo patrocinado e o conteúdo divulgado é de responsabilidade de seus autores.",
+    "photoCover": "https://miro.medium.com/v2/resize:fill:160:107/1*ycbFBGQ-t6iBBZn38vLpGw.jpeg"
+  },
+  {
+    "id": "2",
+    "title": "Entenda a diferença entre arrow function e funções regulares em javascript",
+    "description": "A prova de que no futuro não existirão viagens no tempo é que não estamos sendo visitados pelos viajantes do futuro. Stephen Hawking...",
+    "photoCover": "https://miro.medium.com/v2/resize:fill:224:224/1*OMcRkAMaB68-91PI82e82w.png"
+  },
+  {
+    "id": "3",
+    "title": "Relaxa, ChatGPT não roubar seu emprego de programador",
+    "description": "Ao menos não tão cedo a ponto de se preocupar — Aproveitando o hype imenso que esse assunto tem levantado para trazer uma reflexão...",
+    "photoCover": "https://miro.medium.com/v2/da:true/resize:fill:224:224/0*5tvrzqGxs3T9TIAA"
+  },
+  {
+    "id": "4",
+    "title": "Minha primeira experiência com o React (Native)",
+    "description": "Me perdoa Angular?!",
+    "photoCover": "https://miro.medium.com/v2/resize:fill:160:107/1*WA_9JsyqFkge2HwYKcdJQw.png"
+  },
+]
